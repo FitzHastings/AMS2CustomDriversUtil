7b9edba334ad792ceebd5ca8360290eaf610cfb7
FitzHastings <Inferiordev@gmail.com>
2023-06-08 23:57:10 +0400

    Grid Generator Development

--- src/net/dragondelve/customdriversutil/model/generator/GeneratorSettings.java
+++ src/net/dragondelve/customdriversutil/model/generator/GeneratorSettings.java
@@ -14,20 +14,133 @@
 
 package net.dragondelve.customdriversutil.model.generator;
 
+import javafx.beans.property.*;
 import net.dragondelve.customdriversutil.model.VehicleClass;
 
 /**
- * Encapsulates generator settings that are necessary to generate a new Grid.
+ * Encapsulates generator settings that are necessary to generate a new driver grid.
  */
 public class GeneratorSettings {
     private VehicleClass vehicleClass;
-    private int nDrivers;
-    private boolean reduceGapsOnOvals;
 
-    private double minValue;
-    private double maxValue;
-    private boolean limitAggression;
-    private double aggressionLimit;
-    private boolean bindQualiAndRaceSkills;
-    private double boundSkillsGap;
+    private ValueGenerator valueGenerator;
+
+    private final BooleanProperty blankNames = new SimpleBooleanProperty();
+
+    private final BooleanProperty useNAMeS = new SimpleBooleanProperty();
+
+    private final BooleanProperty fromLiveryNames = new SimpleBooleanProperty();
+
+    private final BooleanProperty bindQualiAndRaceSkills = new SimpleBooleanProperty();
+    private final BooleanProperty reduceGapsOnOvals = new SimpleBooleanProperty();
+    private final BooleanProperty limitAggression = new SimpleBooleanProperty();
+    private final IntegerProperty nDrivers = new SimpleIntegerProperty();
+    private final DoubleProperty minValue = new SimpleDoubleProperty();
+    private final DoubleProperty  maxValue = new SimpleDoubleProperty();
+    private final DoubleProperty aggressionLimit = new SimpleDoubleProperty();
+    private final DoubleProperty boundSkillsGap = new SimpleDoubleProperty();
+
+    public VehicleClass getVehicleClass() {
+        return vehicleClass;
+    }
+
+    public void setVehicleClass(VehicleClass vehicleClass) {
+        this.vehicleClass = vehicleClass;
+    }
+
+    public ValueGenerator getValueGenerator() {
+        return valueGenerator;
+    }
+
+    public void setValueGenerator(ValueGenerator valueGenerator) {
+        this.valueGenerator = valueGenerator;
+    }
+
+    public boolean isBlankNames() {
+        return blankNames.get();
+    }
+
+    public BooleanProperty blankNamesProperty() {
+        return blankNames;
+    }
+
+    public boolean isUseNAMeS() {
+        return useNAMeS.get();
+    }
+
+    public BooleanProperty useNAMeSProperty() {
+        return useNAMeS;
+    }
+
+    public boolean isFromLiveryNames() {
+        return fromLiveryNames.get();
+    }
+
+    public BooleanProperty fromLiveryNamesProperty() {
+        return fromLiveryNames;
+    }
+
+    public boolean isBindQualiAndRaceSkills() {
+        return bindQualiAndRaceSkills.get();
+    }
+
+    public BooleanProperty bindQualiAndRaceSkillsProperty() {
+        return bindQualiAndRaceSkills;
+    }
+
+    public boolean isReduceGapsOnOvals() {
+        return reduceGapsOnOvals.get();
+    }
+
+    public BooleanProperty reduceGapsOnOvalsProperty() {
+        return reduceGapsOnOvals;
+    }
+
+    public boolean isLimitAggression() {
+        return limitAggression.get();
+    }
+
+    public BooleanProperty limitAggressionProperty() {
+        return limitAggression;
+    }
+
+    public int getnDrivers() {
+        return nDrivers.get();
+    }
+
+    public IntegerProperty nDriversProperty() {
+        return nDrivers;
+    }
+
+    public double getMinValue() {
+        return minValue.get();
+    }
+
+    public DoubleProperty minValueProperty() {
+        return minValue;
+    }
+
+    public double getMaxValue() {
+        return maxValue.get();
+    }
+
+    public DoubleProperty maxValueProperty() {
+        return maxValue;
+    }
+
+    public double getAggressionLimit() {
+        return aggressionLimit.get();
+    }
+
+    public DoubleProperty aggressionLimitProperty() {
+        return aggressionLimit;
+    }
+
+    public double getBoundSkillsGap() {
+        return boundSkillsGap.get();
+    }
+
+    public DoubleProperty boundSkillsGapProperty() {
+        return boundSkillsGap;
+    }
 }
